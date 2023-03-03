92784b02c174c7ae9b07490bec0467f1a4b18b08
Hacer-rue <pavlik.soldatov.12@gmail.com>
2023-03-03 13:09:28 +0300

    update

--- mint.js
+++ mint.js
@@ -1,10 +1,20 @@
 const {enroll} = require('./lib/FabricCa')
 const {mintToken, startGateway} = require('./lib/FabricGateway')
 
+const channel = "mychannel"
+const tokenContract = "HLTst2"
+const amount = 12345678900000000
+
 async function main() {
+    var _tokenContract
+    var _amount
+    if(process.argv[2]) _tokenContract = process.argv[2]
+    else _tokenContract = tokenContract
+    if(process.argv[3]) _amount = process.argv[3]
+    else _amount = amount
     const user = await enroll("user1", "user1pw")
     const gateway = await startGateway(user)
-    const result = await mintToken(gateway, "mychannel", "HLTst2", 12345678900000000)
+    const result = await mintToken(gateway, "mychannel", _tokenContract, _amount)
     console.log(Boolean(result))
 }
 
--- newTransfer.js
+++ newTransfer.js
@@ -3,11 +3,25 @@ const {startGateway, transferToken, newTransfer} = require('./lib/FabricGateway'
 
 const releId = "x509::/C=US/ST=North Carolina/O=Hyperledger/OU=admin/CN=org1admin::/C=US/ST=North Carolina/L=Durham/O=org1.example.com/CN=ca.org1.example.com"
 
+const channel = "mychannel"
+const tokenContract = "HLTst2"
+const amount = 123456789000
+const to = "0:e0ba35748c663038889e73d7a25d772092847d910970b8c04ea70adb1a6895a9"
+
 async function main() {
+    var _tokenContract
+    var _amount
+    var _to
+    if(process.argv[2]) _tokenContract = process.argv[2]
+    else _tokenContract = tokenContract
+    if(process.argv[3]) _amount = process.argv[3]
+    else _amount = amount
+    if(process.argv[4]) _to = process.argv[4]
+    else _to = to
     const user = await enroll("user1", "user1pw")
     const gateway = await startGateway(user)
-    const transactionTransfer = await transferToken(gateway, "mychannel", "HLTst2", releId, 123456789000)
-    const transactionNewTransfer = await newTransfer(gateway, "mychannel", "0:e0ba35748c663038889e73d7a25d772092847d910970b8c04ea70adb1a6895a9", 123456789000, "HLTst2", transactionTransfer)
+    const transactionTransfer = await transferToken(gateway, channel, _tokenContract, releId, _amount)
+    const transactionNewTransfer = await newTransfer(gateway, channel, _to, _amount, _tokenContract, transactionTransfer)
     console.log(transactionNewTransfer)
 }
 
