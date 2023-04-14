e6088933f88722f04354df8eee82ded6792d4e8b
Hacer-rue <pavlik.soldatov.12@gmail.com>
2023-04-14 10:16:45 +0300

    update

--- initERC.js
+++ initERC.js
@@ -3,12 +3,18 @@ const { readCertUser, readKeyUser } = require('./lib/utils')
 
 async function main() {
     var _contractName
-    var _name
+    var _name = ""
     var _symbol
     var _decimals
     if(process.argv[2]) _contractName = process.argv[2]
     else throw Error("сontractName cannot be empty")
-    if(process.argv[3]) _name = process.argv[3]
+    if(process.argv[3]) {
+        var split = process.argv[3].split('/')
+        split.forEach((element) => {
+            _name += element + " "
+        })
+        _name = _name.trim()
+    }
     else throw Error("name cannot be empty")
     if(process.argv[4]) _symbol = process.argv[4]
     else throw Error("symbol cannot be empty")
